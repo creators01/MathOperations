9cf84a3635984f968b4ebd43b81913ecb319ca28
masa <creaters645@gmail.com>
2021-02-20 13:43:01 +0530

    Some changes has done now that if any other input is press than it will throw the notification that this input is not handled by this program

--- main.py
+++ main.py
@@ -9,21 +9,32 @@ def MathOperation(num1,num2):
             Return:
                 :return Addition,Substaction,Multiplication and Division
             """
-    inp = input('''What you want to do, 
+    inp = input('''What you want to do, Press q to Quite
 For addition press a!
 For Substraction press s!: 
 For Division Press d!: 
 For Multiplication Press m!: 
 ''')
     if inp == 'a' or inp == 'A':
-        return num1 + num2
+            return num1 + num2
     elif inp == 's' or inp == 'S':
-        return num1 - num2
+            return num1 - num2
     elif inp == 'd' or inp == 'D':
-        return num1/num2
+            return num1/num2
     elif inp == 'm' or inp == 'M':
-        return num1*num2
+            return num1*num2
+    else:
+        print("it is not handled by this program")
+
 test1 = MathOperation(5,10)
 print(test1)
+test2 = MathOperation(0, 0)
+print(test2)
+test3 = MathOperation(4, 50)
+print(test3)
+
+
+
+
 
 
