3650c19944f139aaf89f0e1168db23d0dfdf8557
masa <creaters645@gmail.com>
2021-02-20 14:18:32 +0530

    Program has some new changes and availiblity to take the input from the user without calling the function every time you have to call the function only one time to get the output result.

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-def MathOperation(num1,num2):
+def MathOperation():
     """
-            Purpose:
-                Use the given Value to Addition,Subtraction,Multiplication and Division
-            Pre-Conditions:
-                :param value: the value to be added
-            Post-Conditions:
-                none
-            Return:
-                :return Addition,Substaction,Multiplication and Division
-            """
-    inp = input('''What you want to do, Press q to Quite
-For addition press a!
-For Substraction press s!: 
-For Division Press d!: 
-For Multiplication Press m!: 
-''')
-    if inp == 'a' or inp == 'A':
-            return num1 + num2
-    elif inp == 's' or inp == 'S':
-            return num1 - num2
-    elif inp == 'd' or inp == 'D':
-            return num1/num2
-    elif inp == 'm' or inp == 'M':
-            return num1*num2
-    else:
-        print("it is not handled by this program")
+    Purpose:Use the given Value to Addition,Subtraction,Multiplication and Division
+    Pre-Conditions: Should we value
+    param value: the value to be added
+    Post-Conditions:none
+    Return:
+        :return Addition,Substaction,Multiplication and Division
+    """
+    num1 = int(input("Enter the First number"))
+    num2 = int(input("Enter the Second number"))
 
-test1 = MathOperation(5,10)
-print(test1)
-test2 = MathOperation(0, 0)
-print(test2)
-test3 = MathOperation(4, 50)
-print(test3)
+    while True:
+        inp = input(
+            '''
+            What you want to do, Press q to Quite
+            For addition press a!
+            For Substraction press s!: 
+            For Division Press d!: 
+            For Multiplication Press m!: 
+            ''')
+        if inp == 'q' or inp == 'Q':
+            break
+
+        try:
+            if inp == 'a' or inp == 'A':
+                print(f"The Addition of two numbers is {num1 + num2}")
+            elif inp == 's' or inp == 'S':
+                print(f"The Substraction of two numbers is {num1 - num2}")
+            elif inp == 'd' or inp == 'D':
+                print(f"The Division of two numbers is {num1 / num2}")
+            elif inp == 'm' or inp == 'M':
+                print(f"The Multiplication of two numbers {num1 * num2}")
+        except Exception as e:
+            print(f'You have typed the wrong input is {e}')
 
 
+MathOperation()
 
 
 
