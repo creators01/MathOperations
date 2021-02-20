c724ecb250f0b6529e992f00facf0884354856c6
masa <creaters645@gmail.com>
2021-02-20 13:28:50 +0530

    we have make a many changes in this program MathOperatin that now this program can also divide and multiply. Now in this Program we can do 1. Adddition 2. Subtraction 3. Multiplication 4. Division

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 def MathOperation(num1,num2):
     """
             Purpose:
-                Use the given Value to Addition and Subtaction
+                Use the given Value to Addition,Subtraction,Multiplication and Division
             Pre-Conditions:
                 :param value: the value to be added
             Post-Conditions:
                 none
             Return:
-                :return Addition or Substaction
+                :return Addition,Substaction,Multiplication and Division
             """
-    inp = input('''What you want to do, For addition press a! For Substraction press s!: ''')
+    inp = input('''What you want to do, 
+For addition press a!
+For Substraction press s!: 
+For Division Press d!: 
+For Multiplication Press m!: 
+''')
     if inp == 'a' or inp == 'A':
         return num1 + num2
     elif inp == 's' or inp == 'S':
         return num1 - num2
+    elif inp == 'd' or inp == 'D':
+        return num1/num2
+    elif inp == 'm' or inp == 'M':
+        return num1*num2
 test1 = MathOperation(5,10)
 print(test1)
+
 
